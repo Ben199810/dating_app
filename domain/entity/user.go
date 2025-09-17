eca85b5321314de70e920778f30ce8a6d3275d1f
bing-wei <bing-wei@mail.chungyo.net>
2025-09-17 16:13:56 +0800

    refactors: user management domain

--- domain/entity/user.go
+++ domain/entity/user.go
@@ -1,17 +1,7 @@
 package entity
 
-import (
-	"time"
-
-	"github.com/google/uuid"
-)
-
-type User struct {
-	ID          uuid.UUID  `json:"id"`
-	Username    string     `json:"username"`
-	Email       string     `json:"email"`
-	Password    string     `json:"-"` // 不序列化密碼
-	CreatedAt   time.Time  `json:"created_at"`
-	UpdatedAt   time.Time  `json:"updated_at"`
-	LastLoginAt *time.Time `json:"last_login_at,omitempty"` // 最後登入時間，可能為空
+type UserInformation struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"-"` // 不序列化密碼
 }
--- domain/repository/user_repository.go
+++ domain/repository/user_repository.go
@@ -1,26 +1,12 @@
 package repository
 
-import (
-	"golang_dev_docker/domain/entity"
-)
+import "golang_dev_docker/domain/entity"
 
-// UserRepository 用戶儲存庫接口
-type UserRepository interface {
-	// ExistsByUsername 檢查用戶名是否已存在
+// 定義註冊使用者的實作介面
+type RegisterUser interface {
+	// 先查詢是否有 user 資料
 	ExistsByUsername(username string) (bool, error)
-
-	// ExistsByEmail 檢查電子郵件是否已存在
 	ExistsByEmail(email string) (bool, error)
-
-	// Save 保存用戶
-	Save(user *entity.User) error
-
-	// FindByID 根據ID查找用戶
-	FindByID(id string) (*entity.User, error)
-
-	// FindByUsername 根據用戶名查找用戶
-	FindByUsername(username string) (*entity.User, error)
-
-	// FindByEmail 根據電子郵件查找用戶
-	FindByEmail(email string) (*entity.User, error)
+	// 新增 user 資料到資料庫
+	RegisterUser(user *entity.User) error
 }
